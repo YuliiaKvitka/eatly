c15faae85e176c002e7785e89db7bb82b65d36a5
YuliiaKvitka <modernwebsite21.gmail.com>
2024-07-02 15:24:25 +0300

    responsive src/react/components/Logo.jsx

--- src/react/components/Logo.jsx
+++ src/react/components/Logo.jsx
@@ -4,7 +4,7 @@ import styles from '../../styles/componentsStyles/Logo.module.scss'
 const Logo = () => {
     return (
         <div className={styles["logo"]}>
-            <Image src='/assets/logo/logo.png' alt='logo' width={46} height={43} loading='lazy' />
+            <Image className={styles['logo__image']}   src='/assets/logo/logo.png' alt='logo' width={46} height={43} loading='lazy' />
             <p className={styles['logo__text']}>eatly</p>
             </div>
     )
