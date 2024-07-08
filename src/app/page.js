4237dcb367aeb8124f084705f6ee9e4a710bcf8e
YuliiaKvitka <modernwebsite21.gmail.com>
2024-07-08 17:49:22 +0300

    Responsive Subscribe section

--- src/app/page.js
+++ src/app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
       <Features />
       <TopRestaurants />
       <TopDishes />
-      <Dashboard />
-      <CustomerSay/>
+      {/* <Dashboard /> */}
+      {/* <CustomerSay/> */}
       <Subscribe/>
     </div>
   );
