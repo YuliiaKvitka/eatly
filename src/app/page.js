85378385f392c3e0d24f56b4e8bfca4ade102ff8
YuliiaKvitka <modernwebsite21.gmail.com>
2024-07-08 22:05:07 +0300

    save work

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,24 @@
 //src/app/page.js
 import Hero from "@/react/sections/homePage/Hero";
-import styles from "./page.module.scss";
 import Statistics from "@/react/sections/homePage/Statistics";
 import Features from "@/react/sections/homePage/Features";
 import TopRestaurants from "@/react/sections/homePage/TopRestaurants";
 import TopDishes from "@/react/sections/homePage/TopDishes";
 import Dashboard from "@/react/sections/homePage/Dashboard";
-import FormSubscribe from "@/react/components/FormSubscribe";
 import Subscribe from "@/react/sections/homePage/Subscribe";
 import CustomerSay from "@/react/sections/homePage/CustomerSay";
 
 export default function Home() {
   return (
     <div > 
-      
-      {/* <Hero />
+      <Hero />
       <Statistics />
       <Features />
       <TopRestaurants />
-      <TopDishes /> */}
+      <TopDishes />
       <Dashboard /> 
-       {/* <CustomerSay/>
-      <Subscribe/> */}
-    </div>
+      <CustomerSay/>
+      <Subscribe/>
+    </div> 
   );
 }
